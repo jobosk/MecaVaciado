c1ed7f0a7a2760608f641ec5477a48aa81de469d
jobosk <jfbosca@gmail.com>
2014-08-02 02:10:46 +0200

    Adding files

---
+++ MecaVaciado/FormMain.Designer.cs
@@ -0,0 +1,332 @@
+﻿namespace MecaVaciado
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBoxIsla = new System.Windows.Forms.CheckBox();
+            this.labelBP = new System.Windows.Forms.Label();
+            this.textBoxBP = new System.Windows.Forms.TextBox();
+            this.textBoxAP = new System.Windows.Forms.TextBox();
+            this.labelAP = new System.Windows.Forms.Label();
+            this.labelB = new System.Windows.Forms.Label();
+            this.textBoxB = new System.Windows.Forms.TextBox();
+            this.textBoxA = new System.Windows.Forms.TextBox();
+            this.labelA = new System.Windows.Forms.Label();
+            this.labelYP = new System.Windows.Forms.Label();
+            this.labelY = new System.Windows.Forms.Label();
+            this.textBoxY = new System.Windows.Forms.TextBox();
+            this.textBoxYP = new System.Windows.Forms.TextBox();
+            this.buttonSalir = new System.Windows.Forms.Button();
+            this.buttonCalcular = new System.Windows.Forms.Button();
+            this.labelF = new System.Windows.Forms.Label();
+            this.labelR = new System.Windows.Forms.Label();
+            this.textBoxF = new System.Windows.Forms.TextBox();
+            this.textBoxR = new System.Windows.Forms.TextBox();
+            this.labelXP = new System.Windows.Forms.Label();
+            this.textBoxXP = new System.Windows.Forms.TextBox();
+            this.labelZ = new System.Windows.Forms.Label();
+            this.textBoxZ = new System.Windows.Forms.TextBox();
+            this.textBoxX = new System.Windows.Forms.TextBox();
+            this.labelX = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // checkBoxIsla
+            // 
+            this.checkBoxIsla.AutoSize = true;
+            this.checkBoxIsla.Location = new System.Drawing.Point(158, 93);
+            this.checkBoxIsla.Name = "checkBoxIsla";
+            this.checkBoxIsla.Size = new System.Drawing.Size(42, 17);
+            this.checkBoxIsla.TabIndex = 22;
+            this.checkBoxIsla.Text = "Isla";
+            this.checkBoxIsla.UseVisualStyleBackColor = true;
+            // 
+            // labelBP
+            // 
+            this.labelBP.AutoSize = true;
+            this.labelBP.Location = new System.Drawing.Point(76, 93);
+            this.labelBP.Name = "labelBP";
+            this.labelBP.Size = new System.Drawing.Size(15, 13);
+            this.labelBP.TabIndex = 14;
+            this.labelBP.Text = "b\'";
+            // 
+            // textBoxBP
+            // 
+            this.textBoxBP.Location = new System.Drawing.Point(96, 90);
+            this.textBoxBP.Name = "textBoxBP";
+            this.textBoxBP.Size = new System.Drawing.Size(41, 20);
+            this.textBoxBP.TabIndex = 15;
+            // 
+            // textBoxAP
+            // 
+            this.textBoxAP.Location = new System.Drawing.Point(96, 64);
+            this.textBoxAP.Name = "textBoxAP";
+            this.textBoxAP.Size = new System.Drawing.Size(41, 20);
+            this.textBoxAP.TabIndex = 13;
+            // 
+            // labelAP
+            // 
+            this.labelAP.AutoSize = true;
+            this.labelAP.Location = new System.Drawing.Point(76, 67);
+            this.labelAP.Name = "labelAP";
+            this.labelAP.Size = new System.Drawing.Size(15, 13);
+            this.labelAP.TabIndex = 12;
+            this.labelAP.Text = "a\'";
+            // 
+            // labelB
+            // 
+            this.labelB.AutoSize = true;
+            this.labelB.Location = new System.Drawing.Point(11, 93);
+            this.labelB.Name = "labelB";
+            this.labelB.Size = new System.Drawing.Size(13, 13);
+            this.labelB.TabIndex = 6;
+            this.labelB.Text = "b";
+            // 
+            // textBoxB
+            // 
+            this.textBoxB.Location = new System.Drawing.Point(29, 90);
+            this.textBoxB.Name = "textBoxB";
+            this.textBoxB.Size = new System.Drawing.Size(41, 20);
+            this.textBoxB.TabIndex = 7;
+            // 
+            // textBoxA
+            // 
+            this.textBoxA.Location = new System.Drawing.Point(29, 64);
+            this.textBoxA.Name = "textBoxA";
+            this.textBoxA.Size = new System.Drawing.Size(41, 20);
+            this.textBoxA.TabIndex = 5;
+            // 
+            // labelA
+            // 
+            this.labelA.AutoSize = true;
+            this.labelA.Location = new System.Drawing.Point(11, 68);
+            this.labelA.Name = "labelA";
+            this.labelA.Size = new System.Drawing.Size(13, 13);
+            this.labelA.TabIndex = 4;
+            this.labelA.Text = "a";
+            // 
+            // labelYP
+            // 
+            this.labelYP.AutoSize = true;
+            this.labelYP.Location = new System.Drawing.Point(76, 41);
+            this.labelYP.Name = "labelYP";
+            this.labelYP.Size = new System.Drawing.Size(14, 13);
+            this.labelYP.TabIndex = 10;
+            this.labelYP.Text = "y\'";
+            // 
+            // labelY
+            // 
+            this.labelY.AutoSize = true;
+            this.labelY.Location = new System.Drawing.Point(11, 42);
+            this.labelY.Name = "labelY";
+            this.labelY.Size = new System.Drawing.Size(12, 13);
+            this.labelY.TabIndex = 2;
+            this.labelY.Text = "y";
+            // 
+            // textBoxY
+            // 
+            this.textBoxY.Location = new System.Drawing.Point(29, 38);
+            this.textBoxY.Name = "textBoxY";
+            this.textBoxY.Size = new System.Drawing.Size(41, 20);
+            this.textBoxY.TabIndex = 3;
+            // 
+            // textBoxYP
+            // 
+            this.textBoxYP.Location = new System.Drawing.Point(96, 38);
+            this.textBoxYP.Name = "textBoxYP";
+            this.textBoxYP.Size = new System.Drawing.Size(41, 20);
+            this.textBoxYP.TabIndex = 11;
+            // 
+            // buttonSalir
+            // 
+            this.buttonSalir.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.buttonSalir.Location = new System.Drawing.Point(128, 127);
+            this.buttonSalir.Name = "buttonSalir";
+            this.buttonSalir.Size = new System.Drawing.Size(75, 23);
+            this.buttonSalir.TabIndex = 24;
+            this.buttonSalir.Text = "Salir";
+            this.buttonSalir.UseVisualStyleBackColor = true;
+            this.buttonSalir.Click += new System.EventHandler(this.buttonSalir_Click);
+            // 
+            // buttonCalcular
+            // 
+            this.buttonCalcular.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.buttonCalcular.Location = new System.Drawing.Point(13, 127);
+            this.buttonCalcular.Name = "buttonCalcular";
+            this.buttonCalcular.Size = new System.Drawing.Size(75, 23);
+            this.buttonCalcular.TabIndex = 23;
+            this.buttonCalcular.Text = "Calcular";
+            this.buttonCalcular.UseVisualStyleBackColor = true;
+            this.buttonCalcular.Click += new System.EventHandler(this.buttonCalcular_Click);
+            // 
+            // labelF
+            // 
+            this.labelF.AutoSize = true;
+            this.labelF.Location = new System.Drawing.Point(143, 68);
+            this.labelF.Name = "labelF";
+            this.labelF.Size = new System.Drawing.Size(10, 13);
+            this.labelF.TabIndex = 20;
+            this.labelF.Text = "f";
+            // 
+            // labelR
+            // 
+            this.labelR.AutoSize = true;
+            this.labelR.Location = new System.Drawing.Point(143, 42);
+            this.labelR.Name = "labelR";
+            this.labelR.Size = new System.Drawing.Size(10, 13);
+            this.labelR.TabIndex = 18;
+            this.labelR.Text = "r";
+            // 
+            // textBoxF
+            // 
+            this.textBoxF.Location = new System.Drawing.Point(159, 65);
+            this.textBoxF.Name = "textBoxF";
+            this.textBoxF.Size = new System.Drawing.Size(41, 20);
+            this.textBoxF.TabIndex = 21;
+            // 
+            // textBoxR
+            // 
+            this.textBoxR.Location = new System.Drawing.Point(159, 38);
+            this.textBoxR.Name = "textBoxR";
+            this.textBoxR.Size = new System.Drawing.Size(41, 20);
+            this.textBoxR.TabIndex = 19;
+            // 
+            // labelXP
+            // 
+            this.labelXP.AutoSize = true;
+            this.labelXP.Location = new System.Drawing.Point(76, 15);
+            this.labelXP.Name = "labelXP";
+            this.labelXP.Size = new System.Drawing.Size(14, 13);
+            this.labelXP.TabIndex = 8;
+            this.labelXP.Text = "x\'";
+            // 
+            // textBoxXP
+            // 
+            this.textBoxXP.Location = new System.Drawing.Point(96, 12);
+            this.textBoxXP.Name = "textBoxXP";
+            this.textBoxXP.Size = new System.Drawing.Size(41, 20);
+            this.textBoxXP.TabIndex = 9;
+            // 
+            // labelZ
+            // 
+            this.labelZ.AutoSize = true;
+            this.labelZ.Location = new System.Drawing.Point(143, 15);
+            this.labelZ.Name = "labelZ";
+            this.labelZ.Size = new System.Drawing.Size(12, 13);
+            this.labelZ.TabIndex = 16;
+            this.labelZ.Text = "z";
+            // 
+            // textBoxZ
+            // 
+            this.textBoxZ.Location = new System.Drawing.Point(159, 12);
+            this.textBoxZ.Name = "textBoxZ";
+            this.textBoxZ.Size = new System.Drawing.Size(41, 20);
+            this.textBoxZ.TabIndex = 17;
+            // 
+            // textBoxX
+            // 
+            this.textBoxX.Location = new System.Drawing.Point(29, 12);
+            this.textBoxX.Name = "textBoxX";
+            this.textBoxX.Size = new System.Drawing.Size(41, 20);
+            this.textBoxX.TabIndex = 1;
+            // 
+            // labelX
+            // 
+            this.labelX.AutoSize = true;
+            this.labelX.Location = new System.Drawing.Point(11, 15);
+            this.labelX.Name = "labelX";
+            this.labelX.Size = new System.Drawing.Size(12, 13);
+            this.labelX.TabIndex = 0;
+            this.labelX.Text = "x";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(214, 162);
+            this.Controls.Add(this.checkBoxIsla);
+            this.Controls.Add(this.labelBP);
+            this.Controls.Add(this.textBoxBP);
+            this.Controls.Add(this.textBoxAP);
+            this.Controls.Add(this.labelAP);
+            this.Controls.Add(this.labelB);
+            this.Controls.Add(this.textBoxB);
+            this.Controls.Add(this.textBoxA);
+            this.Controls.Add(this.labelA);
+            this.Controls.Add(this.labelYP);
+            this.Controls.Add(this.labelY);
+            this.Controls.Add(this.textBoxY);
+            this.Controls.Add(this.textBoxYP);
+            this.Controls.Add(this.buttonSalir);
+            this.Controls.Add(this.buttonCalcular);
+            this.Controls.Add(this.labelF);
+            this.Controls.Add(this.labelR);
+            this.Controls.Add(this.textBoxF);
+            this.Controls.Add(this.textBoxR);
+            this.Controls.Add(this.labelXP);
+            this.Controls.Add(this.textBoxXP);
+            this.Controls.Add(this.labelZ);
+            this.Controls.Add(this.textBoxZ);
+            this.Controls.Add(this.textBoxX);
+            this.Controls.Add(this.labelX);
+            this.MinimumSize = new System.Drawing.Size(230, 200);
+            this.Name = "FormMain";
+            this.Text = "MecaVaciado";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBoxIsla;
+        private System.Windows.Forms.Label labelBP;
+        private System.Windows.Forms.TextBox textBoxBP;
+        private System.Windows.Forms.TextBox textBoxAP;
+        private System.Windows.Forms.Label labelAP;
+        private System.Windows.Forms.Label labelB;
+        private System.Windows.Forms.TextBox textBoxB;
+        private System.Windows.Forms.TextBox textBoxA;
+        private System.Windows.Forms.Label labelA;
+        private System.Windows.Forms.Label labelYP;
+        private System.Windows.Forms.Label labelY;
+        private System.Windows.Forms.TextBox textBoxY;
+        private System.Windows.Forms.TextBox textBoxYP;
+        private System.Windows.Forms.Button buttonSalir;
+        private System.Windows.Forms.Button buttonCalcular;
+        private System.Windows.Forms.Label labelF;
+        private System.Windows.Forms.Label labelR;
+        private System.Windows.Forms.TextBox textBoxF;
+        private System.Windows.Forms.TextBox textBoxR;
+        private System.Windows.Forms.Label labelXP;
+        private System.Windows.Forms.TextBox textBoxXP;
+        private System.Windows.Forms.Label labelZ;
+        private System.Windows.Forms.TextBox textBoxZ;
+        private System.Windows.Forms.TextBox textBoxX;
+        private System.Windows.Forms.Label labelX;
+    }
+}
---
+++ MecaVaciado/FormMain.cs
@@ -0,0 +1,319 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Globalization;
+
+namespace MecaVaciado
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void calcular(Double x, Double y, Double a, Double b, Double xp, Double yp, Double ap, Double bp, Double z, Double r, Double f, Boolean isla)
+        {
+            Double zo = 0;
+
+            // Algun dato es incorrecto
+            if (a <= 0 || b <= 0 || z >= zo || r <= 0 || f <= 0)
+                return;
+
+            // La herramienta no cabe en el hueco
+            if ((2 * r) > a || (2 * r) > b)
+                return;
+
+            Int32 nv = (int) (Math.Abs(z) / f);
+            Double rv = Math.Abs(z) % f;
+
+            Int32 nh = (int) (a / (2 * r));
+            Double rh = a % (2 * r);
+
+            if (isla)
+            {
+                // Algun dato es incorrecto
+                if (ap <= 0 || bp <= 0)
+                    return;
+
+                // La isla está fuera del hueco en el eje de las X
+                if (xp <= x || xp >= (x + a))
+                    return;
+
+                // La isla esta fuera del hueco en el eje de las Y
+                if (yp <= y || yp >= (y + b))
+                    return;
+
+                // La isla es más grande que el hueco en alguna dimension
+                if (ap > a || bp > b)
+                    return;
+
+                // La herramienta no cabe en el hueco entre la pared y la isla
+                if ((2 * r) > (xp - x) || (2 * r) > (yp - y))
+                    return;
+            }
+
+            SaveFileDialog salvar = new SaveFileDialog();
+
+            //salvar.Filter = "Archivos ISO (*.iso)|*.iso";
+            //salvar.Filter = "Archivos DXF (*.dxf)|*.dxf";
+            salvar.Filter = "Archivos de texto (*.txt)|*.txt";
+            salvar.FilterIndex = 2;
+
+            if (salvar.ShowDialog() != DialogResult.OK)
+                return;
+
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(salvar.FileName, false))
+            {
+                //file.WriteLine("N10 G54");
+
+                //file.WriteLine("N20 G17 G90 G71 G94");
+
+                //file.WriteLine("N30 T1 D1 M06 S2000 F400");
+
+                //file.WriteLine("N40 M3");
+
+                Int32 n = 10;
+
+                Double i = x + r;
+                Double j = y + r;
+                Double k = 0;
+
+                file.WriteLine("N" + n + " G00 X" + i.ToString(CultureInfo.InvariantCulture) + " Y" + j.ToString(CultureInfo.InvariantCulture) + " Z" + zo.ToString(CultureInfo.InvariantCulture));
+                n += 5;
+
+                // Contorno del hueco
+                
+                for (int p = 0; p <= nv; p++)
+                {
+                    if (p == nv)
+                    {
+                        if (rv == 0)
+                            break;
+
+                        k -= rv;
+                    }
+                    else
+                        k -= f;
+
+                    file.WriteLine("N" + n + " G01 X" + i.ToString(CultureInfo.InvariantCulture) + " Y" + j.ToString(CultureInfo.InvariantCulture) + " Z" + k.ToString(CultureInfo.InvariantCulture));
+                    n += 5;
+
+                    i += a - (2 * r);
+
+                    file.WriteLine("N" + n + " G01 X" + i.ToString(CultureInfo.InvariantCulture) + " Y" + j.ToString(CultureInfo.InvariantCulture) + " Z" + k.ToString(CultureInfo.InvariantCulture));
+                    n += 5;
+
+                    j += b - (2 * r);
+
+                    file.WriteLine("N" + n + " G01 X" + i.ToString(CultureInfo.InvariantCulture) + " Y" + j.ToString(CultureInfo.InvariantCulture) + " Z" + k.ToString(CultureInfo.InvariantCulture));
+                    n += 5;
+
+                    i -= a - (2 * r);
+
+                    file.WriteLine("N" + n + " G01 X" + i.ToString(CultureInfo.InvariantCulture) + " Y" + j.ToString(CultureInfo.InvariantCulture) + " Z" + k.ToString(CultureInfo.InvariantCulture));
+                    n += 5;
+
+                    j -= b - (2 * r);
+
+                    file.WriteLine("N" + n + " G01 X" + i.ToString(CultureInfo.InvariantCulture) + " Y" + j.ToString(CultureInfo.InvariantCulture) + " Z" + k.ToString(CultureInfo.InvariantCulture));
+                    n += 5;
+                }
+
+                file.WriteLine("N" + n + " G01 X" + i.ToString(CultureInfo.InvariantCulture) + " Y" + j.ToString(CultureInfo.InvariantCulture) + " Z" + zo.ToString(CultureInfo.InvariantCulture));
+                n += 5;
+
+                if (isla)
+                {
+                    i = xp - r;
+                    j = yp - r;
+
+                    file.WriteLine("N" + n + " G00 X" + i.ToString(CultureInfo.InvariantCulture) + " Y" + j.ToString(CultureInfo.InvariantCulture) + " Z" + zo.ToString(CultureInfo.InvariantCulture));
+                    n += 5;
+
+                    // Contorno de la isla
+
+                    k = 0;
+
+                    for (int p = 0; p <= nv; p++)
+                    {
+
+                        if (p == nv)
+                        {
+                            if (rv == 0)
+                                break;
+
+                            k -= rv;
+                        }
+                        else
+                            k -= f;
+
+                        file.WriteLine("N" + n + " G01 X" + i.ToString(CultureInfo.InvariantCulture) + " Y" + j.ToString(CultureInfo.InvariantCulture) + " Z" + k.ToString(CultureInfo.InvariantCulture));
+                        n += 5;
+
+                        i += ap + (2 * r);
+
+                        file.WriteLine("N" + n + " G01 X" + i.ToString(CultureInfo.InvariantCulture) + " Y" + j.ToString(CultureInfo.InvariantCulture) + " Z" + k.ToString(CultureInfo.InvariantCulture));
+                        n += 5;
+
+                        j += bp + (2 * r);
+
+                        file.WriteLine("N" + n + " G01 X" + i.ToString(CultureInfo.InvariantCulture) + " Y" + j.ToString(CultureInfo.InvariantCulture) + " Z" + k.ToString(CultureInfo.InvariantCulture));
+                        n += 5;
+
+                        i -= ap + (2 * r);
+
+                        file.WriteLine("N" + n + " G01 X" + i.ToString(CultureInfo.InvariantCulture) + " Y" + j.ToString(CultureInfo.InvariantCulture) + " Z" + k.ToString(CultureInfo.InvariantCulture));
+                        n += 5;
+
+                        j -= bp + (2 * r);
+
+                        file.WriteLine("N" + n + " G01 X" + i.ToString(CultureInfo.InvariantCulture) + " Y" + j.ToString(CultureInfo.InvariantCulture) + " Z" + k.ToString(CultureInfo.InvariantCulture));
+                        n += 5;
+                    }
+
+                    file.WriteLine("N" + n + " G01 X" + i.ToString(CultureInfo.InvariantCulture) + " Y" + j.ToString(CultureInfo.InvariantCulture) + " Z" + zo.ToString(CultureInfo.InvariantCulture));
+                    n += 5;
+                }
+
+                // Vaciado
+
+                k = 0;
+
+                for (int p = 0; p <= nv; p++)
+                {
+                    i = x + r;
+                    j = y + r;
+
+                    bool dir = true;
+
+                    file.WriteLine("N" + n + " G00 X" + i.ToString(CultureInfo.InvariantCulture) + " Y" + j.ToString(CultureInfo.InvariantCulture) + " Z" + zo.ToString(CultureInfo.InvariantCulture));
+                    n += 5;
+
+                    if (p == nv)
+                    {
+                        if (rv == 0)
+                            break;
+
+                        k -= rv;
+                    }
+                    else
+                        k -= f;
+
+                    for (int q = 0; q <= nh; q++)
+                    {
+                        if (q == nh)
+                        {
+                            if (rh == 0)
+                                break;
+
+                            i += rh;
+                        }
+                        else if (q > 0)
+                            i += (2 * r);
+
+                        file.WriteLine("N" + n + " G01 X" + i.ToString(CultureInfo.InvariantCulture) + " Y" + j.ToString(CultureInfo.InvariantCulture) + " Z" + k.ToString(CultureInfo.InvariantCulture));
+                        n += 5;
+
+                        if (dir)
+                        {
+                            j += b - (2 * r);
+
+                            if (isla && i > (xp - r) && i < (xp + ap + r))
+                            {
+                                file.WriteLine("N" + n + " G01 X" + i.ToString(CultureInfo.InvariantCulture) + " Y" + (yp - r).ToString(CultureInfo.InvariantCulture) + " Z" + k.ToString(CultureInfo.InvariantCulture));
+                                n += 5;
+
+                                file.WriteLine("N" + n + " G01 X" + i.ToString(CultureInfo.InvariantCulture) + " Y" + (yp - r).ToString(CultureInfo.InvariantCulture) + " Z" + zo.ToString(CultureInfo.InvariantCulture));
+                                n += 5;
+
+                                file.WriteLine("N" + n + " G01 X" + i.ToString(CultureInfo.InvariantCulture) + " Y" + (yp + bp + r).ToString(CultureInfo.InvariantCulture) + " Z" + zo.ToString(CultureInfo.InvariantCulture));
+                                n += 5;
+
+                                file.WriteLine("N" + n + " G01 X" + i.ToString(CultureInfo.InvariantCulture) + " Y" + (yp + bp + r).ToString(CultureInfo.InvariantCulture) + " Z" + k.ToString(CultureInfo.InvariantCulture));
+                                n += 5;
+                            }
+                        }
+                        else
+                        {
+                            j -= b - (2 * r);
+
+                            if (isla && i > (xp - r) && i < (xp + ap + r))
+                            {
+                                file.WriteLine("N" + n + " G01 X" + i.ToString(CultureInfo.InvariantCulture) + " Y" + (yp + bp + r).ToString(CultureInfo.InvariantCulture) + " Z" + k.ToString(CultureInfo.InvariantCulture));
+                                n += 5;
+
+                                file.WriteLine("N" + n + " G01 X" + i.ToString(CultureInfo.InvariantCulture) + " Y" + (yp + bp + r).ToString(CultureInfo.InvariantCulture) + " Z" + zo.ToString(CultureInfo.InvariantCulture));
+                                n += 5;
+
+                                file.WriteLine("N" + n + " G01 X" + i.ToString(CultureInfo.InvariantCulture) + " Y" + (yp - r).ToString(CultureInfo.InvariantCulture) + " Z" + zo.ToString(CultureInfo.InvariantCulture));
+                                n += 5;
+
+                                file.WriteLine("N" + n + " G01 X" + i.ToString(CultureInfo.InvariantCulture) + " Y" + (yp - r).ToString(CultureInfo.InvariantCulture) + " Z" + k.ToString(CultureInfo.InvariantCulture));
+                                n += 5;
+                            }
+                        }
+
+                        dir = !dir;
+
+                        file.WriteLine("N" + n + " G01 X" + i.ToString(CultureInfo.InvariantCulture) + " Y" + j.ToString(CultureInfo.InvariantCulture) + " Z" + k.ToString(CultureInfo.InvariantCulture));
+                        n += 5;
+                    }
+
+                    file.WriteLine("N" + n + " G01 X" + i.ToString(CultureInfo.InvariantCulture) + " Y" + j.ToString(CultureInfo.InvariantCulture) + " Z" + zo.ToString(CultureInfo.InvariantCulture));
+                    n += 5;
+                }
+
+                //file.WriteLine("N" + n + " M5");
+                //n += 5;
+
+                //file.WriteLine("N" + n + " M30");
+                //n += 5;
+            }
+        }
+
+        private void buttonCalcular_Click(object sender, EventArgs e)
+        {
+            Double x, y, a, b;
+            Double xp, yp, ap, bp;
+            Double z, r, f;
+
+            Boolean isla;
+
+            Double.TryParse(this.textBoxX.Text, out x);
+            Double.TryParse(this.textBoxY.Text, out y);
+            Double.TryParse(this.textBoxA.Text, out a);
+            Double.TryParse(this.textBoxB.Text, out b);
+
+            isla = this.checkBoxIsla.Checked;
+
+            if (isla)
+            {
+                Double.TryParse(this.textBoxXP.Text, out xp);
+                Double.TryParse(this.textBoxYP.Text, out yp);
+                Double.TryParse(this.textBoxAP.Text, out ap);
+                Double.TryParse(this.textBoxBP.Text, out bp);
+            }
+            else
+            {
+                xp = yp = ap = bp = 0.0;
+            }
+
+            Double.TryParse(this.textBoxZ.Text, out z);
+            Double.TryParse(this.textBoxR.Text, out r);
+            Double.TryParse(this.textBoxF.Text, out f);
+
+            calcular(x, y, a, b, xp, yp, ap, bp, z, r, f, isla);
+        }
+
+        private void buttonSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
